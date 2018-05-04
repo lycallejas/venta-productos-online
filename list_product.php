b95ecac2a99c48eae490ad9a1fe1f702e810412e
lycallejas <37189458+lycallejas@users.noreply.github.com>
2018-05-04 17:55:54 -0500

    Update list_product.php

--- list_product.php
+++ list_product.php
@@ -19,8 +19,7 @@
 	<div class="navbar navbar-inverse navbar-fixed-top">
 		<div class="container-fluid">	
 			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
-					<span class="sr-only">navigation</span>
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">					<span class="sr-only">navigation</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
