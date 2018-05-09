1956afee0b65da2a76490c178e7cc9ac02f14e90
MariaPaulaS <37190986+MariaPaulaS@users.noreply.github.com>
2018-05-09 12:45:07 -0500

    Update list_product.php

--- list_product.php
+++ list_product.php
@@ -20,3 +20,127 @@ if(!$_SESSION["rol"] == "admin"){
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<style></style>
 	</head>
+<body style="background-color:black; ">
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
+					<span class="sr-only">navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="register_product.php" class="navbar-brand">Registrar Producto</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="list_client.php"><span class="glyphicon glyphicon-home"></span>Listado Clientes</a></li>
+				<li><a href="#"><span class="glyphicon glyphicon-modal-window"></span>Listado Producto</a></li>
+			</ul>
+			<form class="navbar-form navbar-left" method ="post">
+		        <div class="form-group">
+		          <input type="text" class="form-control" placeholder="Search" id="search" onKeyUp="buscar();">
+		        </div>
+		        
+		     </form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-shopping-cart"></span>Cart<span class="badge">0</span></a>
+					<div class="dropdown-menu" style="width:400px;">
+						<div class="panel panel-success">
+							<div class="panel-heading">
+								<div class="row">
+									<div class="col-md-3">Sl.No</div>
+									<div class="col-md-3">Imagen del Producto</div>
+									<div class="col-md-3">Nombre Producto</div>
+									<div class="col-md-3">Precio en $</div>
+								</div>
+							</div>
+							<div class="panel-body">
+								<div id="cart_product">
+								<!--<div class="row">
+									<div class="col-md-3">Sl.No</div>
+									<div class="col-md-3">Product Image</div>
+									<div class="col-md-3">Product Name</div>
+									<div class="col-md-3">Price in $.</div>
+								</div>-->
+								</div>
+							</div>
+							<div class="panel-footer"></div>
+						</div>
+					</div>
+				</li>
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span><?php echo "Hi,".$_SESSION["name"]; ?></a>
+					<ul class="dropdown-menu">
+						<li><a href="#" style="text-decoration:none; color:blue;"><span class="glyphicon glyphicon-shopping-cart">Cart</a></li>
+						<li class="divider"></li>
+						<li><a href="#" style="text-decoration:none; color:blue;">Ordenes</a></li>
+						<li class="divider"></li>
+						<li><a href="" style="text-decoration:none; color:blue;">Chnage Password</a></li>
+						<li class="divider"></li>
+						<li><a href="logout.php" style="text-decoration:none; color:blue;">Logout</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>	
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-1"></div>
+			<div class="col-md-2 col-xs-12">
+				<div id="get_category">
+				</div>
+				<!--<div class="nav nav-pills nav-stacked">
+					<li class="active"><a href="#"><h4>Categories</h4></a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+				</div> -->
+				<div id="get_brand">
+				</div>
+				<!--<div class="nav nav-pills nav-stacked">
+					<li class="active"><a href="#"><h4>Brand</h4></a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+					<li><a href="#">Categories</a></li>
+				</div> -->
+			</div>
+			<div class="col-md-8 col-xs-12">
+				<div class="row">
+					<div class="col-md-12 col-xs-12" id="product_msg">
+					</div>
+				</div>
+				<div class="panel panel-info">
+					<div class="panel-heading">Productos</div>
+					<div class="panel-body">
+						<div id="gets_product">
+							
+
+						</div>
+					</div>
+					<div class="panel-footer">&copy; 2018</div>
+				</div>
+			</div>
+			<div class="col-md-1"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<center>
+					<ul class="pagination" id="pagen_pro">
+						<li><a href="#">1</a></li>
+					</ul>
+				</center>
+			</div>
+		</div>
+	</div>
+</body>
+
+
