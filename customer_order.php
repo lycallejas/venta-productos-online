ac58eeeadbed9a178202625466d5d8d1c9bc0b7f
lycallejas <37189458+lycallejas@users.noreply.github.com>
2018-05-22 20:50:44 -0500

    Create customer_order.php

---
+++ customer_order.php
@@ -0,0 +1,49 @@
+<?php
+///SPRING 3
+error_reporting(E_ALL);
+	ini_set('display_errors', '1');
+session_start();
+
+if(!$_SESSION["rol"] == "cliente"){
+	header("location:index.php");
+}
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Carro Compra</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/main2.js"></script>
+		<style>
+			table tr td {padding:10px;}
+		</style>
+	</head>
+<body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<a href="#" class="navbar-brand">Khan Store</a>
+			</div>
+			<ul class="nav navbar-nav">
+				<li><a href="index.php"><span class="glyphicon glyphicon-home"></span>Home</a></li>
+				<li><a href="index.php"><span class="glyphicon glyphicon-modal-window"></span>Productos</a></li>
+			</ul>
+		</div>
+	</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+	
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<div class="panel panel-default">
+					<div class="panel-heading"></div>
+					<div class="panel-body">
+						<h1>Detalles de Ordenes</h1>
+						<hr/>
